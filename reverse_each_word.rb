2313f1318584776eb76dbd62efbc784b7118e5e8
Kailana Kahawaii <kailanakahawaii@gmail.com>
2020-01-05 23:57:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(sentence)
   new_sentence = sentence.split 
   reversed_new_sentence = new_sentence.collect { |n|  n.reverse }
   
- new_sentence.
+reversed_new_sentence.to_s
 end 
